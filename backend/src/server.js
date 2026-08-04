5f102adf6be0cd0a948886565ae227367f9a2ddc
kankana das <kankanadas2482@gmail.com>
2026-08-04 20:53:25 +0530

    Fix CORS for deployed frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from 'dotenv';
 dotenv.config();
-console.log("JWT:", process.env.JWT_SECRET_KEY);
+// console.log("JWT:", process.env.JWT_SECRET_KEY);
 import cors from "cors";
 import path from "path";
 import authRoutes from './routes/auth.route.js'
@@ -15,8 +15,11 @@ const app=express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true,//allow frontend to send the cookies
+    origin: [
+        "http://localhost:5173",
+        "https://chatnova-pi.vercel.app"
+    ],
+    credentials: true,
 }));
 app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
