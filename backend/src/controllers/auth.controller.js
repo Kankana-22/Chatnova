59d16d62655fd5125638f91483a0425ee042aa2a
kankana das <kankanadas2482@gmail.com>
2026-08-04 20:58:06 +0530

    Fix auth cookies for production

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -76,11 +76,11 @@ export async function signup(req, res) {
 
 
     res.cookie("jwt", token, {
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-      httpOnly: true,
-      sameSite: "strict",
-      secure: process.env.NODE_ENV === "production",
-    });
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  httpOnly: true,
+  sameSite: "none",
+  secure: true,
+});
 
 
     res.status(201).json({
@@ -178,7 +178,12 @@ export async function login(req, res) {
 
 export function logout(req,res){
 
-  res.clearCookie("jwt");
+  // res.clearCookie("jwt");
+  res.clearCookie("jwt", {
+  httpOnly: true,
+  sameSite: "none",
+  secure: true,
+});
 
   res.status(200).json({
     success:true,
